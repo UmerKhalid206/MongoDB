537fb32fca9b6144cd92c3bed7148de3dfe8124c
UmerKhalid206 <100032318+UmerKhalid206@users.noreply.github.com>
2024-09-15 15:50:14 +0500

    insert and insertMany

--- Server/index.js
+++ Server/index.js
@@ -42,5 +42,116 @@ const getDatabases = async() => {
 
 getDatabases()
 
+//.............................................................................
+/*
+CRUD operations using mongoDB
+*/
 
+/*
+Note:  Mongoose VS simply accessing mongoDB 
+
+1- Schema Definition and Validation:
+MongoDB is schema-less, meaning it does not enforce any structure on documents within a collection.
+Mongoose allows you to define schemas for your collections, which makes it easier to enforce structure and validation rules on your data.
+With Mongoose, you can define required fields, default values, data types (e.g., String, Number), and custom validation logic.
+
+2- Data Relationships (Object References):
+MongoDB doesn’t natively support deep relationships between collections like SQL databases (though you can use manual references or embedded documents).
+Mongoose allows for easier management of relationships between documents using ObjectIds to reference documents in other collections. It provides built-in support for population, which enables you to join related data more seamlessly.
+
+3- Middleware (Hooks)
+Mongoose provides middleware (or hooks) that allows you to perform actions before or after certain database operations (e.g., save, remove, update). This is very useful for things like logging, authentication, data validation, or triggering other actions in the lifecycle of your data.
+MongoDB driver doesn’t provide this functionality out of the box
+
+Much more than these 3 points
+*/
+
+// insertOne()
+/*
+Note: if sample_analytics does not exist then mongoDB would create a database named 
+"sample_analytics" automatically and then insert document into it
+*/
+
+// client.db('sample_analytics').collection('users').insertOne({
+//     name: 'Umer',
+//     age: 23
+// }).then((res) => console.log('res =>', res))
+
+//...........................................................................
+//insertMany()
+
+// const collection = client.db('sample_analytics').collection('users')
+
+// collection.insertMany([
+//     {
+//       student_id: 1,
+//       subjects: [
+//         {
+//           type: "quiz",
+//           score: 50,
+//         },
+//         {
+//           type: "homework",
+//           score: 70,
+//         },
+//         {
+//           type: "quiz",
+//           score: 66,
+//         },
+//         {
+//           type: "exam",
+//           score: 70,
+//         },
+//       ],
+//       class_id: 551,
+//     },
+//     {
+//       student_id: 2,
+//       subjects: [
+//         {
+//           type: "exam",
+//           score: 83,
+//         },
+//         {
+//           type: "quiz",
+//           score: 59,
+//         },
+//         {
+//           type: "quiz",
+//           score: 72,
+//         },
+//         {
+//           type: "quiz",
+//           score: 67,
+//         },
+//       ],
+//       class_id: 550,
+//     },
+//     {
+//       student_id: 3,
+//       subjects: [
+//         {
+//           type: "exam",
+//           score: 45,
+//         },
+//         {
+//           type: "homework",
+//           score: 39,
+//         },
+//         {
+//           type: "quiz",
+//           score: 40,
+//         },
+//         {
+//           type: "homework",
+//           score: 88,
+//         },
+//       ],
+//       class_id: 551,
+//     },
+//   ])
+
+
+
+//...........................................................................
 
