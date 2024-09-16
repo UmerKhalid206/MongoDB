9151a1e420d6bef19ea6619563f469404f0c026f
UmerKhalid206 <100032318+UmerKhalid206@users.noreply.github.com>
2024-09-16 22:29:58 +0500

    logical $and $or operators

--- Server/index.js
+++ Server/index.js
@@ -280,12 +280,97 @@ you can also use elemMatch for multiple array values
 //     equal to one (means match all three conditions) and that query would return all
 //     those documents who have an items array that contains further document with 
 //     condition => {name: "laptop", price: {$gt: 800}, quantity: {$gte: 1}}*/
+
 //     $elemMatch: {name: "laptop", price: {$gt: 800}, quantity: {$gte: 1}}
 // }})
 // .limit(2).toArray()
 // .then((arr)=> console.log(JSON.stringify(arr,null, 2)))   //more readable form
 
 //............................................................................
+
+
+//logical operators in MongoDB
+
+/*
+$and => db.collection.find({<expression>, <expression>}) //this syntax's comma is similar to $and operation
+$or
+*/
+
+//Example of $and
+
+// client.db('sample_training').collection('routes')
+// .find({// find airline with name Aerocondor and airline with alias 2B and that main document have 0 or more stops
+//     $and: [{"airline.name": "Aerocondor"}, {"airline.alias": "2B"}, {"stops": {$gte: 0}}]
+// }).limit(2).toArray()
+// .then((arr)=> console.log(JSON.stringify(arr,null, 2)))   //more readable form
+
+
+// you can achieve implicit $and functionality without using $and
+
+// client.db('sample_training').collection('routes')
+// .find({// find airline with name Aerocondor and airline with alias 2B and that main document have 0 or more stops
+//    "airline.name": "Aerocondor", "airline.alias": "2B", "stops": {$gte: 0}
+// }).limit(2).toArray()
+// .then((arr)=> console.log(JSON.stringify(arr,null, 2)))
+
+
+//.....................................................................
+
+// $or operator
+// client.db('sample_training').collection('routes')
+// .find({ //find those routes documents that contain SEA airport as destination or source airport
+//     $or: [{"dst_airport": "SEA"}, {"src_airport": "SEA"}]
+// }).limit(25).toArray()
+// .then((arr)=> console.log(JSON.stringify(arr,null, 2)))
+
+//...............................................................
+
+//you can also use $and $or operators together
+
+//Example
+// client.db('sample_training').collection('routes')
+// .find({
+//     $and:[
+//         {$or:[ //either dst_airport or src_airport should be "SEA"
+//             {dst_airport: "SEA"},
+//             {src_airport: "SEA"}
+//         ]},
+//         {$or:[ //either airline.name="American Airlines"  or airplane should be 320
+//             {"airline.name": "American Airlines"},
+//             {airplane: 320}
+//         ]}
+
+//     ]  
+//     /*above query would pick a document that must fullfil both the or operators 
+//     which would result in $and operation*/
+// }).limit(2).toArray()
+// .then((arr)=> console.log(JSON.stringify(arr,null, 2)))
+
+
+//......................................................................
+
+// using two $or operators without $and
+//what if we use two $or operators without $and
+
+/*it would not produce expected results because in the output you can check
+it does not get the document with source or destination airport to be "SEA"
+and the reason for that is: 
+Our first $or expression, the incoming and outgoing flights was overwritten by
+the subsequent $or expression which includes airline name or airplane,
+
+this happened because we can't store two fields with same name in the same JSON
+object 
+
+As a general rule, when including the same operator more than once in your query,
+you need to use the explicit $and operator
+*/
+
+// client.db('sample_training').collection('routes')
+// .find({  
+//     $or: [{dst_airport: "SEA"}, {src_airport: "SEA"}],
+//     $or: [{"airline.name": "American Airlines"}, {airplane: 320}]    
+// }).limit(2).toArray()
+// .then((arr)=> console.log(JSON.stringify(arr,null, 2)))
 
 
 
